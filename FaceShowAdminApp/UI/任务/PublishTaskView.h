0796f5db887c463baa9697c4cae62f4bdf1c79d3
xiaolong <xiaolong@qq.com>
2018-06-11 14:19:13 +0800

    提交测试

---
+++ FaceShowAdminApp/UI/任务/PublishTaskView.h
@@ -0,0 +1,13 @@
+//
+//  PublishTaskView.h
+//  FaceShowAdminApp
+//
+//  Created by 郑小龙 on 2018/6/11.
+//  Copyright © 2018年 niuzhaowang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PublishTaskView : UIView
+
+@end
---
+++ FaceShowAdminApp/UI/任务/PublishTaskView.m
@@ -0,0 +1,21 @@
+//
+//  PublishTaskView.m
+//  FaceShowAdminApp
+//
+//  Created by 郑小龙 on 2018/6/11.
+//  Copyright © 2018年 niuzhaowang. All rights reserved.
+//
+
+#import "PublishTaskView.h"
+
+@implementation PublishTaskView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
