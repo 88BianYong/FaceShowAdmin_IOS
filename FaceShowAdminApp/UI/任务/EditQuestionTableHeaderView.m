bc4bd91c35ba7062d197d4c1ee26b1e6a47e9afc
xiaolong <xiaolong@qq.com>
2018-06-22 17:06:39 +0800

    添加错误删除

--- FaceShowAdminApp/UI/任务/EditQuestionTableHeaderView.m
+++ FaceShowAdminApp/UI/任务/EditQuestionTableHeaderView.m
@@ -47,6 +47,7 @@
         self.thirdView.selected = NO;
         BLOCK_EXEC(self.questionTypeBlock,@"1");
     }];
+    self.firstView.selected = YES;
     self.firstView.titleString = @"单选";
     [self addSubview:self.firstView];
     self.secondView = [[EditQuestionTypeView alloc] init];
