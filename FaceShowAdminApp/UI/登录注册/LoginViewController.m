7f3fc014032440024fec52afd2e1de7a368d9d0b
LeeXueHao <lixuehao@yanxiu.com>
2018-11-07 10:37:22 +0800

    fix

--- FaceShowAdminApp/UI/登录注册/LoginViewController.m
+++ FaceShowAdminApp/UI/登录注册/LoginViewController.m
@@ -155,7 +155,9 @@
         [UIView animateWithDuration:0.3 animations:^{
             [self.view layoutIfNeeded];
         }completion:^(BOOL finished) {
-            [self.inputView setFocus];
+            if (self.scrollView.contentInset.bottom > 0) {
+                [self.inputView setFocus];
+            }
         }];
     }];
 
@@ -173,7 +175,9 @@
         [UIView animateWithDuration:0.3 animations:^{
             [self.view layoutIfNeeded];
         }completion:^(BOOL finished) {
-            [self.inputView setFocus];
+            if (self.scrollView.contentInset.bottom > 0) {
+                [self.inputView setFocus];
+            }
         }];
     }];
 
